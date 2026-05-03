e43b4da1f9908b7dc0ead058fa1c8a67541354a5
shixiang <shixiang5968@gmail.com>
2026-05-03 16:06:14 +0800

    delete unnecessary files

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ecommerce_system.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
