73f48447a861042f4023b5b697c4ce3a4ee9b3ba
shixiang <shixiang5968@gmail.com>
2026-05-03 16:22:18 +0800

    fix bug

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -11,11 +11,4 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<User> Users { get; set; }
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<OrderItem> OrderItems { get; set; }
-    public DbSet<Payment> Payments { get; set; }
-    public DbSet<SupportRequest> SupportRequests { get; set; }
-    public DbSet<Notification> Notifications { get; set; }
 }
