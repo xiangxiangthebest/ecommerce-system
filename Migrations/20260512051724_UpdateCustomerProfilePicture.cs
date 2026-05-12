e0d99dc6d9fec5268e3bc848b4fe2b06918e5524
Vickygay <vicky05pong@gmail.com>
2026-05-12 16:40:47 +0800

    Add Product

---
+++ Migrations/20260512051724_UpdateCustomerProfilePicture.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCustomerProfilePicture : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicture",
+                table: "Customers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicture",
+                table: "Customers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,5 +5,6 @@ public class Customer : User
     public string Address { get; set; } = string.Empty;
     public string Gender { get; set; } = string.Empty;
     public DateTime? Birthday { get; set; }
-    public string ProfilePicture { get; set; }
+    public string? ProfilePicture { get; set; }
 }
+
